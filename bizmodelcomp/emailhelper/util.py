67e642df528112b42faf600601464c78625c77a6
Rob Fitzpatrick <robftz@gmail.com>
2010-10-27 02:11:30 +0100

    email tweaks

--- bizmodelcomp/emailhelper/util.py
+++ bizmodelcomp/emailhelper/util.py
@@ -79,7 +79,7 @@ def send_email(subject, message_markdown, to_email, from_email=None):
     # and message to send - here it is sent as one string.
     result = s.sendmail(from_email, to_email, msg.as_string())
     #send a copy of every email to rob for debugging
-    result_debug = s.sendmail(from_email, "robftz+nvanadebug@gmail.com", msmg.as_string())
+    #result_debug = s.sendmail(from_email, "robftz+nvanadebug@gmail.com", msmg.as_string())
 
     f.write("       sent mail with result=%s\n\n\n" % str(result))
 
