32ed36ae86d6d8b3baaa18c2ff584744e108c421
Rob Fitzpatrick <robftz@gmail.com>
2010-11-25 09:43:23 +0000

    faster sorting

--- bizmodelcomp/competition/models.py
+++ bizmodelcomp/competition/models.py
@@ -108,21 +108,24 @@ class Competition(models.Model):
     def pitches(self):
 
         unsorted = self.current_phase.pitches.all()
-        sorted_pitches = []
-    
-        for u in unsorted:
-            inserted = False
-            
-            for s in sorted_pitches:
-                if u.average_score() >= s.average_score():
-                    sorted_pitches.insert(sorted_pitches.index(s), u)
-                    inserted = True
-                    break
+        
+##        sorted_pitches = []
+##    
+##        for u in unsorted:
+##            inserted = False
+##            
+##            for s in sorted_pitches:
+##                if u.average_score() >= s.average_score():
+##                    sorted_pitches.insert(sorted_pitches.index(s), u)
+##                    inserted = True
+##                    break
+##
+##            if not inserted:
+##                sorted_pitches.append(u)
+##
+##        return sorted_pitches
 
-            if not inserted:
-                sorted_pitches.append(u)
-
-        return sorted_pitches
+        return unsorted
     
 
     def is_judging_open(self):
