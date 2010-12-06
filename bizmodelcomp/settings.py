9a6e283054c3a67f1301c09b53d20257a79e27eb
server <=>
2010-12-06 00:34:24 +0000

    changed database

--- bizmodelcomp/settings.py
+++ bizmodelcomp/settings.py
@@ -39,7 +39,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'mysql'       # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'django_db'     # Or path to database file if using sqlite3.
+DATABASE_NAME = 'django_db_4'     # Or path to database file if using sqlite3.
 DATABASE_USER = 'root'          # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
