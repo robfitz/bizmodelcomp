634464cda1f8af6e1c0e05e0b6d36a8275bc33d3
Rob Fitzpatrick <robftz@gmail.com>
2010-12-20 10:26:04 +0000

    combo login & register form, hopefully making life easier

--- bizmodelcomp/userhelper/urls.py
+++ bizmodelcomp/userhelper/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^login/$', 'userhelper.views.loginUser'),
+    (r'^login/$', 'userhelper.views.loginRegister'),
     (r'^logout/$', 'userhelper.views.logoutUser'),
-    (r'^register/$', 'userhelper.views.registerUser'),
+    (r'^register/$', 'userhelper.views.loginRegister'),
     (r'^no_permissions/$', 'userhelper.views.noPermissions'),
     (r'^verify_email/$', 'userhelper.views.verify_email'),
 )
--- bizmodelcomp/userhelper/views.py
+++ bizmodelcomp/userhelper/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib import auth
-from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth import *
 from django.contrib.auth.models import User
 
 from settings import ACCOUNT_EMAIL_CONFIRM_REQUIRED
@@ -12,9 +12,142 @@ from utils.util import rand_key
 
 from competition.models import *
 
+
+
 def noPermissions(request):
 
     return render_to_response('userhelper/no_permissions.html')
+
+
+
+def loginRegister(request):
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/dashboard/')
+
+    alert = None
+
+    if request.method == "POST" and len(request.POST) > 0:
+        
+        login_or_register = request.POST.get("login_or_register", "login")
+
+        if login_or_register == "login":
+
+            email = request.POST.get("email", None)
+            password = request.POST.get("password", None)
+
+            user = auth.authenticate(username=email, password=password)
+
+            if user is not None:
+
+                print 'user is not none! %s, %s' % (request, user)
+
+                #logged in successfully
+                auth.login(request, user)
+
+                next = request.POST.get("next", None)
+                print 'got next: %s' % next
+
+                if not next:
+
+                    print 'length of judge invitations: %s' % JudgeInvitation.objects.filter(user=user)
+
+                    if len(JudgeInvitation.objects.filter(user=user)) > 0:
+                        next = "/judge/"
+                    else:
+                        next = "/dashboard/"
+                
+                return HttpResponseRedirect(next)
+            
+            else:
+                #login failed
+                alert = "Login failed. Try again?"
+
+
+        elif login_or_register == "register":
+
+            email = request.POST["email"]
+            pass1 = request.POST["password1"]
+            pass2 = request.POST["password2"]
+                
+            user = None
+            try:
+                user = createNewUser(request, email, pass1, pass2)
+            except:
+                next = request.POST.get("next")
+                login = "/accounts/login/"
+                if next:
+                    login += "?next=%s" % next
+                alert = "That email has already been registered. Try <a href='%s'>logging in</a> instead?" % login
+
+            try:
+                #is there a verification key already hooked up to that email?
+                key = VerificationKey.objects.get(email=user.email)
+                key.user = user
+                key.save()
+            except: pass
+
+            if user is not None:
+
+                is_judge = is_new_user_judge(user)
+                print 'so was it a judge? %s' % is_judge
+
+                if is_judge:
+                    next = "/judge/"
+                else: 
+                    next = request.POST.get("next")
+                    if not next:
+                        #if we don't have an explicitly set next page, we take our best guess
+                        next = "/dashboard/"
+
+                print 'go here next: %s' % next
+
+                #require email confirmation?
+                if ACCOUNT_EMAIL_CONFIRM_REQUIRED:
+                    user.is_active = False
+                    user.save()
+
+                    #send a verification link for them to clicky click
+                    try:
+                        key = VerificationKey.objects.get(user=user)
+
+                        if key.is_verified:
+                            #this email has already been verified somehow,
+                            #like clicking on an email-only link. so we don't
+                            #need to check it again. yays.
+                            user.is_active = True
+                            user.save()
+                            return HttpResponseRedirect(next)
+                        
+                        else:
+                            send_verification_email(user, next)
+                    except:
+                        send_verification_email(user, next)
+
+                    #display a page telling them what's going on
+                    return HttpResponseRedirect("/accounts/verify_email/?next=%s" % next)
+                
+                #successfully made a user and don't require email confirmation, 
+                #so redirect to next page
+                return HttpResponseRedirect(next)
+                                                
+            elif not alert:
+                #if we failed to make a user and don't have a more specific alert, 
+                #go with a generic fail message
+                alert = "Passwords don't match or your email isn't valid. Try again?"
+
+
+    elif request.method == "GET":
+        #page to redirect to after success
+        next = request.GET.get("next", None)
+
+        email = request.GET.get("e", "")
+
+        #if they got to the judging page from the email link,
+        #we can verify their email right now
+        got_ev_key(request.GET.get("ev", False))
+                
+    return render_to_response('userhelper/login_register.html', locals())
 
 
 
@@ -188,27 +321,28 @@ def verify_email(request):
 def createNewUser(request, email, pass1, pass2, account_type="default", discount_code=None):
 
     if email is not None and pass1 == pass2:
-            user = User.objects.create_user(username=email,
-                                            email=email,
-                                            password = pass1)
+        user = User.objects.create_user(username=email,
+                                        email=email,
+                                        password = pass1)
+        if user is not None:
+            user.save()
+            user = auth.authenticate(username=email, password=pass1)            
+
             if user is not None:
-                user.save()
-                user = auth.authenticate(username=email, password=pass1)            
-                if user is not None:
-                    user_info = UserProfile(user=user)
-		    user_info.save()
+                user_info = UserProfile(user=user)
+                user_info.save()
                     
-		    #successful login
-                    login(request, user)
+                #successful login
+                login(request, user)
 
-                    return user
+                return user
 
-                else:
-                    print "We couldn't log in to your new account. Try again?"
-                    return None
             else:
-                print "We couldn't create your account successfully. Try again?"
+                print "We couldn't log in to your new account. Try again?"
                 return None
+        else:
+            print "We couldn't create your account successfully. Try again?"
+            return None
     else:
         print "Create new user big 'else'"
         return None
