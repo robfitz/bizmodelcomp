d13c128e3cd4c20b825176299f0e22534c18e6f8
Rob Fitzpatrick <robftz@gmail.com>
2011-01-28 06:04:51 +0000

    Popups for displaying static copy text

--- bizmodelcomp/sitecopy/views.py
+++ bizmodelcomp/sitecopy/views.py
@@ -2,6 +2,15 @@ from sitecopy.models import SiteCopy
 from django.shortcuts import get_object_or_404, render_to_response
 
 
+def static_copy_popup(request, site_copy_id):
+
+    title = site_copy_id
+
+    copy = get_object_or_404(SiteCopy, id=site_copy_id).text
+
+    return render_to_response('sitecopy/static_copy_popup.html', locals())
+
+
 
 def static_copy(request, site_copy_id):
 
