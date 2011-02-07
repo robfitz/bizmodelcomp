0fd1ec285332c97e2f0a4e2d77ddfd8ad85af5d4
tstonez <thomasrorystone@gmail.com>
2011-02-07 11:52:45 +0000

    update admin.py files to make admin more functional

--- bizmodelcomp/blog/admin.py
+++ bizmodelcomp/blog/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from bizmodelcomp.blog.models import *
  
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('custom_title',)
+    list_display = ('custom_title', 'url', 'created',)
+    list_filter = ('categories',)
     filter_horizontal = ('categories',)
 
 admin.site.register(Category)
