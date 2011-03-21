b65ea1de51e9042fa1a2cbc28b9d872bb086335a
Rob Fitzpatrick <robftz@gmail.com>
2011-03-21 13:36:22 +0000

    Removed missing view from analytics urls

--- bizmodelcomp/analytics/urls.py
+++ bizmodelcomp/analytics/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
 
-    #landing page
-    (r'^(?P<comp_url>[-_:!()@#$%* a-zA-z0-9]{1,100})/$',
-        'analytics.views.dashboard'),
-
     #popups
     (r'^table/(?P<comp_url>[-_:!()@#$%* a-zA-z0-9]{1,100})/$',
         'analytics.views.table'),
