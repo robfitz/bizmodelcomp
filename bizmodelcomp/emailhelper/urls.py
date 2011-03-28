8209dd7868cf2c2bee0e0af9a2470a805a8783a2
Rob Fitzpatrick <robftz@gmail.com>
2011-03-28 14:32:19 +0100

    Re-enabled email pages of dashboard

--- bizmodelcomp/emailhelper/urls.py
+++ bizmodelcomp/emailhelper/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 
 
     #TODO: remove this
-    (r'', 'competition.views.brb_org'),
+    #(r'', 'competition.views.brb_org'),
     
     (r'^(?P<comp_url>[-_:!()@#$%* a-zA-z0-9]{1,100})/confirm/(?P<bulk_email_id>[0-9]{1,100})/$', 'emailhelper.views.confirm_send_email'),
 
